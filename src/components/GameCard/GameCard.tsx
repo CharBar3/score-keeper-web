30996dd1edd62ad9fd84a15c85265227da79a284
Charles Bartlett <ownageunit@gmail.com>
2023-05-23 13:46:48 -0400

    fixed a bug where the nav bar was not on top of everything.
    gamecards now have a flip animation on render

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,6 +1,6 @@
 // "use client";
 import { Color } from "@/models";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, keyframes } from "@mui/material";
 import { FC, useEffect, useRef, useState } from "react";
 import GameIcon from "../../../public/game-icons/cards_icon.svg";
 import PlayerIcon from "../../../public/icons/player_icon_55px.svg";
@@ -11,13 +11,59 @@ interface GameCardProps {
   title: string;
   color: Color;
   numberOfPlayers: number;
+  index: number;
 }
 
-const GameCard: FC<GameCardProps> = ({ title, numberOfPlayers, color }) => {
+const growKeys = keyframes`
+  0% {
+    opacity: 0;
+    width: 0%;
+
+  }
+  100% {
+    opacity: 1;
+    width: 100%;
+  }
+  `;
+const flipOuterKeys = keyframes`
+  0% {
+    transform: scale(-1, 1);
+  }
+  100% {
+    transform: scale(1, 1);
+  }
+  `;
+const flipInnerKeys = keyframes`
+  0% {
+    opacity: 1; 
+  }
+  30% {
+    opacity: 1;
+  }
+  31% {
+    opacity: 0; 
+  }
+  100% {
+    opacity: 0;
+  }
+  `;
+
+// transform: translate(0) scale(0);
+// transform: translate(200px, 200px) scale(1);
+
+const GameCard: FC<GameCardProps> = ({
+  title,
+  numberOfPlayers,
+  color,
+  index,
+}) => {
   const gameCardRef = useRef<HTMLDivElement>(null);
   const [borderWidth, setBorderWidth] = useState<number | null>(null);
   const [shadowHeight, setShadowHeight] = useState<number | null>(null);
   const [textHeight, setTextHeight] = useState<number | null>(null);
+
+  const animationDelay = (index + 1) * 0.15;
+  const flipTime = ".3s";
 
   const borderColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
   const boxShadowColor = `rgb(${color.red * 0.8}, ${color.green * 0.8}, ${
@@ -42,61 +88,80 @@ const GameCard: FC<GameCardProps> = ({ title, numberOfPlayers, color }) => {
 
   return (
     <Box
-      ref={gameCardRef}
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        borderStyle: "solid",
-        borderRadius: "7px",
-        border: `${borderWidth}px solid ${borderColor}`,
-        boxShadow: `0px ${shadowHeight}px ${boxShadowColor}`,
-        marginBottom: `${shadowHeight}px`,
-        aspectRatio: "13/14",
+        animation: `${flipOuterKeys} ${flipTime}`,
+        animationDelay: `${animationDelay}s`,
       }}
     >
       <Box
+        ref={gameCardRef}
         sx={{
-          paddingTop: 1,
-          height: "20%",
+          position: "relative",
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          borderStyle: "solid",
+          borderRadius: "7px",
+          border: `${borderWidth}px solid ${borderColor}`,
+          boxShadow: `0px ${shadowHeight}px ${boxShadowColor}`,
+          marginBottom: `${shadowHeight}px`,
+          aspectRatio: "13/14",
         }}
       >
-        <Typography
-          textAlign="center"
-          // noWrap
+        <Box
           sx={{
-            fontSize: `${textHeight ? textHeight / 10 : 10}px`,
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            zIndex: 1,
+            backgroundColor: `${borderColor}`,
+            animation: `${flipInnerKeys} ${flipTime} forwards`,
+            animationDelay: `${animationDelay}s`,
+          }}
+        ></Box>
+        <Box
+          sx={{
+            paddingTop: 1,
+            height: "20%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          {title}
-        </Typography>
-      </Box>
-      {numberOfPlayers === 0 ? (
-        <PlusIcon height="60%" color={borderColor} />
-      ) : (
-        <GameIcon height="60%" color={borderColor} />
-      )}
-
-      <Box sx={{ height: "10%", display: "flex" }}>
-        {numberOfPlayers != 0 && (
           <Typography
+            textAlign="center"
+            // noWrap
             sx={{
               fontSize: `${textHeight ? textHeight / 10 : 10}px`,
-              paddingLeft: 1,
-              paddingRight: "4px",
-              display: "flex",
-              alignItems: "center",
             }}
           >
-            {numberOfPlayers}
+            {title}
           </Typography>
+        </Box>
+        {numberOfPlayers === 0 ? (
+          <PlusIcon height="60%" color={borderColor} />
+        ) : (
+          <GameIcon height="60%" color={borderColor} />
         )}
-        {numberOfPlayers != 0 && (
-          <PlayerIcon height="90%" color={theme.palette.primary.main} />
-        )}
+
+        <Box sx={{ height: "10%", display: "flex" }}>
+          {numberOfPlayers != 0 && (
+            <Typography
+              sx={{
+                fontSize: `${textHeight ? textHeight / 10 : 10}px`,
+                paddingLeft: 1,
+                paddingRight: "4px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {numberOfPlayers}
+            </Typography>
+          )}
+          {numberOfPlayers != 0 && (
+            <PlayerIcon height="90%" color={theme.palette.primary.main} />
+          )}
+        </Box>
       </Box>
     </Box>
   );
--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -7,18 +7,39 @@ import Link from "next/link";
 import { FC, useState } from "react";
 import GameCard from "../GameCard/GameCard";
 import SearchBar from "../SearchBar/SearchBar";
-import { useTheme } from "@mui/material/styles";
+import { keyframes, styled, useTheme } from "@mui/material/styles";
 import {
   Box,
   Button,
   CircularProgress,
   Collapse,
+  Grow,
+  Slide,
   Stack,
   Typography,
 } from "@mui/material";
 import PlusIcon from "../../../public/icons/plus_icon_55px.svg";
+import { TransitionGroup } from "react-transition-group";
 
 interface GameListProps {}
+
+const growAnimation = keyframes`
+0% {
+  opacity: 0;
+  // transform: translate(0) scale(0);
+  width: 0%
+}
+100% {
+  opacity: 1;
+  width: 100%
+  // transform: translate(200px, 200px) scale(1);
+}
+`;
+
+// const StyledGridItem = styled(Grid)(({ theme }) => ({
+//   backgroundColor: "red",
+//   animation: `${growAnimation} 2s`,
+// }));
 
 const GameList: FC<GameListProps> = () => {
   const { gameList } = useDataStore();
@@ -33,14 +54,15 @@ const GameList: FC<GameListProps> = () => {
       .filter(({ title }) =>
         title.toLowerCase().includes(gameFilter.toLowerCase())
       )
-      .map(({ id, title, color, players }) => {
+      .map(({ id, title, color, players }, index) => {
         return (
-          <Grid key={id} xs={6} sm={6} md={4} lg={3} xl={3}>
+          <Grid key={id} xs={6} sm={6} md={4}>
             <Link href={`dashboard/game/${id}`}>
               <GameCard
                 title={title}
                 color={color}
                 numberOfPlayers={players.length}
+                index={index}
               />
             </Link>
           </Grid>
@@ -59,45 +81,45 @@ const GameList: FC<GameListProps> = () => {
   };
 
   return (
-    <Stack spacing={1} sx={{ marginBottom: "56px" }}>
-      <Grid container spacing={{ xs: 2, sm: 2, md: 2, lg: 3, xl: 3 }}>
-        <Grid xs={9}>
-          <SearchBar
-            placeholder="Search Games"
-            onChangeSearch={filterGames}
-            debounce={null}
-          />
-        </Grid>
-        <Grid xs={3}>
-          <Link href="dashboard/game/new">
-            <Button
-              variant="dark"
-              sx={{
-                minWidth: "0px",
-                margin: "0px 0px 8px 0px",
-                width: "100%",
-                height: "40px",
-              }}
-            >
-              <PlusIcon height="100%" />
-            </Button>
-          </Link>
-        </Grid>
-        <Grid xs={12}>
-          <Collapse in={!showGames}>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "32px",
-              }}
-            >
-              <Typography>Loading</Typography>
-              <CircularProgress size={"24px"} sx={{ marginLeft: 1 }} />
-            </Box>
-          </Collapse>
-        </Grid>
+    <Stack spacing={1} sx={{ maxWidth: "900px", margin: "auto" }}>
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{ flexGrow: 1, padding: "0px 8px" }}
+      >
+        <SearchBar
+          placeholder="Search Games"
+          onChangeSearch={filterGames}
+          debounce={null}
+        />
+        <Link href="dashboard/game/new">
+          <Button
+            variant="dark"
+            sx={{
+              minWidth: "0px",
+              margin: "0px 0px 8px 0px",
+              width: "100%",
+              height: "40px",
+            }}
+          >
+            <PlusIcon height="100%" />
+          </Button>
+        </Link>
+      </Stack>
+      <Collapse in={!showGames}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "32px",
+          }}
+        >
+          <Typography>Loading</Typography>
+          <CircularProgress size={"24px"} sx={{ marginLeft: 1 }} />
+        </Box>
+      </Collapse>
+      <Grid container spacing={{ xs: 2 }}>
         {showGames}
       </Grid>
     </Stack>
